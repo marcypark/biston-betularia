10db0688cabbe6e19daef45c6c7f0eda46f64f32
Marcella Park <marcellajpark@gmail.com>
2018-12-13 01:45:56 -0500

    have you scene the white peppered moth

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const ROUND_DURATION = 10000;
+const ROUND_DURATION = 5000;
+var round = 1;
+var BB_N = 1;
+var Bb_N = 4;
+var bb_N = 5; // start in Hardy-Weinberg equilibrium: p = 0.3
 var info;
 var timer;
 var melanics_n = 0;
 var nonmelanics_n = 0;
 var total_n = 0;
-var play = false; // could use this to differentiate between scene types
 
 var SceneGamePlay = new Phaser.Class({
     Extends: Phaser.Scene,
@@ -25,16 +28,38 @@ var SceneIntro = new Phaser.Class({
         {
             Phaser.Scene.call(this, { key: 'sceneIntro' });
         },
-    preload: function () {
-        this.load.image('logo', 'assets/nonmelanic_large.png');
+    create: function () {
+		const nextButton = this.add.text(600, 600, 'Next', { fill: '#87ceeb' });
+		nextButton.setInteractive();
+
+		nextButton.on('pointerup', () => this.scene.start('sceneGamePlay'));
+    }
+});
+
+var SceneGameOver = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+        function SceneGameOver ()
+        {
+            Phaser.Scene.call(this, { key: 'sceneGameOver' });
+        },
+	preload: function () {
+        this.load.image('nonmelanic_large', 'assets/nonmelanic_large.png');
     },
     create: function () {
-        this.add.image (0, 0, 'logo');
-		setTimeout(() => this.scene.start('sceneGamePlay'), 3000);
-    },
-    update: function () {
-
-    }
+		this.add.image(400, 200, 'nonmelanic_large');
+		const congrats = this.add.text(400, 450, 'Nice job!', { fontSize: '30px', fill: '#87ceeb' })
+		congrats.setOrigin(0.5);
+		const info = this.add.text(400, 550,
+	        'melanics: ' + melanics_n +
+	        '    non-melanics: ' + nonmelanics_n +
+	        '    total: ' + total_n,
+			{ fill: '#87ceeb' }
+		);
+		info.setOrigin(0.5);
+		const thanks = this.add.text(400, 580, 'Thank you for playing! Ctrl/Cmd+R to play again.', { fill: '#87ceeb' });
+		thanks.setOrigin(0.5);
+	}
 });
 
 var config = {
@@ -48,7 +73,7 @@ var config = {
             debug: false
         }
     },
-    scene: [ SceneIntro, SceneGamePlay ]
+    scene: [ SceneIntro, SceneGamePlay, SceneGameOver ]
 };
 
 var game = new Phaser.Game(config);
@@ -63,12 +88,16 @@ function preloadGamePlay ()
 
 function createGamePlay ()
 {
-    this.add.image(400, 400, 'lichenless');
+	if (round == 1 || round == 4 || round == 5 && Math.random() <= 0.5) {
+		this.add.image(400, 400, 'lichen');
+	} else {
+		this.add.image(400, 400, 'lichenless');
+	}
 
     melanics = this.physics.add.staticGroup();
     nonmelanics = this.physics.add.staticGroup();
 
-    for (var i = 0, len = 11; i < len; i++) {
+    for (var i = 0; i < BB_N + Bb_N; i++) {
         melanic = melanics.create(
             Phaser.Math.RND.between(0, 800), Phaser.Math.RND.between(0, 800),
             'melanic');
@@ -76,7 +105,7 @@ function createGamePlay ()
         melanic.on('clicked', collectMelanic, this);
     }
 
-    for (var i = 0, len = 11; i < len; i++) {
+    for (var i = 0; i < bb_N; i++) {
         nonmelanic = nonmelanics.create(
             Phaser.Math.RND.between(0, 800), Phaser.Math.RND.between(0, 800),
             'nonmelanic');
@@ -89,7 +118,7 @@ function createGamePlay ()
         gameObject.emit('clicked', gameObject);
     }, this);
 
-    info = this.add.text(10, 10, '', { font: '20px Arial', fill: '#87ceeb' });
+    info = this.add.text(10, 10, '', { fill: '#87ceeb' });
     timer = this.time.addEvent({ delay: ROUND_DURATION, callback: roundOver, callbackScope: this });
 }
 
@@ -99,7 +128,8 @@ function updateGamePlay ()
         'melanics: ' + melanics_n +
         '    non-melanics: ' + nonmelanics_n +
         '\ntime remaining: ' + Math.floor(ROUND_DURATION - timer.getElapsed()) +
-        '    total: ' + total_n);
+        '    total: ' + total_n
+	);
 }
 
 function collectMelanic (melanic)
@@ -109,6 +139,11 @@ function collectMelanic (melanic)
     melanic.setVisible(false);
     melanics_n ++;
     total_n ++;
+	if (Math.random() <= BB_N / (BB_N + Bb_N)) {
+		BB_N --;
+	} else {
+		Bb_N --;
+	}
 }
 
 // TODO: Refactor this
@@ -119,11 +154,78 @@ function collectNonMelanic (nonmelanic)
     nonmelanic.setVisible(false);
     nonmelanics_n ++;
     total_n ++;
+	bb_N --;
 }
 
 function roundOver ()
 {
     this.input.off('gameobjectup');
-    // inter-round stuff here
-    // this.scene.restart();
+	round ++;
+    multiply();
+	if (round <= 5) {
+		this.scene.restart();
+	} else {
+		this.scene.start('sceneGameOver');
+	}
+
+}
+
+function multiply ()
+{
+	var p_BB_N = BB_N;
+	var p_Bb_N = Bb_N;
+	var p_bb_N = bb_N;
+	var p_total_N = p_BB_N + p_Bb_N + p_bb_N;
+	while (p_total_N >= 2) {
+		var p1;
+		if (Math.random() <= p_BB_N / p_total_N) {
+			p1 = 'BB';
+			p_BB_N --;
+		} else if (Math.random() <= p_Bb_N / (p_Bb_N + p_bb_N)) {
+			p1 = 'Bb';
+			p_Bb_N --;
+		} else {
+			p1 = 'bb';
+			p_bb_N --;
+		}
+		p_total_N -= 1;
+		var p2;
+		if (Math.random() <= p_BB_N / p_total_N) {
+			p2 = 'BB';
+			p_BB_N --;
+		} else if (Math.random() <= p_Bb_N / (p_Bb_N + p_bb_N)) {
+			p2 = 'Bb';
+			p_Bb_N --;
+		} else {
+			p2 = 'bb';
+			p_bb_N --;
+		}
+		p_total_N -= 1;
+
+		if (p1 == 'BB' && p2 == 'BB') {
+			BB_N ++;
+		} else if (p1 == 'bb' && p2 == 'bb') {
+			bb_N ++;
+		} else if (p1 == 'BB' && p2 == 'bb' || p1 == 'bb' && p2 == 'BB') {
+			if (Math.random() <= 0.5) {
+				Bb_N ++;
+			} else if (Math.random() <= 0.5) {
+				BB_N ++;
+			} else {
+				bb_N ++;
+			}
+		} else if (p1 == 'BB' && p2 == 'Bb' || p1 == 'Bb' && p2 == 'BB') {
+			if (Math.random() <= 0.5) {
+				BB_N ++;
+			} else {
+				Bb_N ++;
+			}
+		} else {
+			if (Math.random() <= 0.5) {
+				Bb_N ++;
+			} else {
+				bb_N ++;
+			}
+		}
+	}
 }
